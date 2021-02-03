6bd9da45186c9ad9f6871508b940bac3656506ae
Luan <ngluanit98@gmail.com>
2021-02-03 13:41:10 +0700

    add

---
+++ app/src/main/java/com/example/miui_ify/FloatingWindow.java
@@ -0,0 +1,266 @@
+package com.example.miui_ify;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.os.Build;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.settingapp.R;
+
+import java.util.Set;
+
+public class FloatingWindow extends Service {
+
+    private Context mContext;
+    private WindowManager mWindowManager;
+    private View mView;
+    private SeekBar seekBar;
+    //Variable to store brightness value
+    private int brightness;
+    //Content resolver used as a handle to the system's settings
+    private ContentResolver cResolver;
+    //Window object, that will store a reference to the current window
+    private Window window;
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        allAboutLayout(intent);
+        moveView();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+
+        if (mView != null) {
+            mWindowManager.removeView(mView);
+        }
+        super.onDestroy();
+    }
+
+    WindowManager.LayoutParams mWindowsParams;
+    private void moveView() {
+        DisplayMetrics metrics = mContext.getResources().getDisplayMetrics();
+        int width = (int) (metrics.widthPixels * 0.7f);
+        int height = (int) (metrics.heightPixels * 0.45f);
+        mWindowsParams = new WindowManager.LayoutParams(
+                width,//WindowManager.LayoutParams.WRAP_CONTENT,
+                height,//WindowManager.LayoutParams.WRAP_CONTENT,
+                //WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+
+                (Build.VERSION.SDK_INT <= 25) ? WindowManager.LayoutParams.TYPE_PHONE : WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+                ,
+
+                //WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH, // Not displaying keyboard on bg activity's EditText
+                //WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, //Not work with EditText on keyboard
+                PixelFormat.TRANSLUCENT);
+
+
+        mWindowsParams.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
+        //params.x = 0;
+        mWindowsParams.y = 100;
+        mWindowManager.addView(mView, mWindowsParams);
+
+        mView.setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            long startTime = System.currentTimeMillis();
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (System.currentTimeMillis() - startTime <= 300) {
+                    return false;
+                }
+                if (isViewInBounds(mView, (int) (event.getRawX()), (int) (event.getRawY()))) {
+                    editTextReceiveFocus();
+                } else {
+                    editTextDontReceiveFocus();
+                }
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = mWindowsParams.x;
+                        initialY = mWindowsParams.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        mWindowsParams.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        mWindowsParams.y = initialY + (int) (event.getRawY() - initialTouchY);
+                        mWindowManager.updateViewLayout(mView, mWindowsParams);
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    private boolean isViewInBounds(View view, int x, int y) {
+        Rect outRect = new Rect();
+        int[] location = new int[2];
+        view.getDrawingRect(outRect);
+        view.getLocationOnScreen(location);
+        outRect.offset(location[0], location[1]);
+        return outRect.contains(x, y);
+    }
+
+    private void editTextReceiveFocus() {
+        if (!wasInFocus) {
+            mWindowsParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
+            mWindowManager.updateViewLayout(mView, mWindowsParams);
+            wasInFocus = true;
+        }
+    }
+
+    private void editTextDontReceiveFocus() {
+        if (wasInFocus) {
+            mWindowsParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
+            mWindowManager.updateViewLayout(mView, mWindowsParams);
+            wasInFocus = false;
+            hideKeyboard(mContext, edt1);
+        }
+    }
+
+    private boolean wasInFocus = true;
+    private EditText edt1;
+    private void allAboutLayout(Intent intent) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mView = layoutInflater.inflate(R.layout.ovelay_window, null);
+        Button btnClose = (Button) mView.findViewById(R.id.btnClose);
+        seekBar = (SeekBar) mView.findViewById(R.id.BrightBar);
+        cResolver =  getContentResolver();
+        BrightnessControl(seekBar);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopSelf();
+
+               // seekBar.setVisibility(View.VISIBLE);
+            }
+        });
+
+    }
+    private void BrightnessControl(SeekBar seekBar) {
+        //Set the seekbar range between 0 and 255
+        //seek bar settings//
+        //sets the range between 0 and 255
+        seekBar.setMax(255);
+        //set the seek bar progress to 1
+        seekBar.setKeyProgressIncrement(1);
+
+        try
+        {
+            //Get the current system brightness
+            brightness = Settings.System.getInt(cResolver, Settings.System.SCREEN_BRIGHTNESS);
+        }
+        catch (Settings.SettingNotFoundException e)
+        {
+            //Throw an error case it couldn't be retrieved
+            Log.e("Error", "Cannot access system brightness");
+            e.printStackTrace();
+        }
+
+        //Set the progress of the seek bar based on the system's brightness
+        seekBar.setProgress(brightness);
+
+        //Register OnSeekBarChangeListener, so it can actually change values
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+                //Set the system brightness using the brightness variable value
+                Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+                //Get the current window attributes
+               // WindowManager.LayoutParams layoutpars = window.getAttributes();
+                //Set the brightness of this window
+                //layoutpars.screenBrightness = brightness / (float)255;
+                //Apply attribute changes to this window
+                //window.setAttributes(layoutpars);
+            }
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+                //Nothing handled here
+            }
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                //Set the minimal brightness level
+                //if seek bar is 20 or any value below
+                if(progress<=20)
+                {
+                    //Set the brightness to 20
+                    brightness=20;
+                }
+                else //brightness is greater than 20
+                {
+                    //Set brightness variable based on the progress bar
+                    brightness = progress;
+                }
+                //Calculate the brightness percentage
+                float perc = (brightness /(float)255)*100;
+                //Set the brightness percentage
+              //  txtPerc.setText((int)perc +" %");
+            }
+        });
+    }
+
+    private void hideKeyboard(Context context, View view) {
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    public void showSoftKeyboard(View view) {
+        if (view.requestFocus()) {
+            InputMethodManager imm = (InputMethodManager)
+                    getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+        }
+    }
+
+}
+
