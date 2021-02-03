11cc93574c62e79a0fae14b91bffe17c9bd9b3d6
thanhbinh <httbinhahihi@gmail.com>
2021-02-03 15:21:30 +0700

    ahihe

--- app/src/main/java/com/example/settingapp/handles/HandlesActivity.kt
+++ app/src/main/java/com/example/settingapp/handles/HandlesActivity.kt
@@ -1,12 +1,23 @@
 package com.example.settingapp.handles
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.example.settingapp.R
 
 class HandlesActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_handles)
+        val window = window
+        getWindow().decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR //  set status text dark
+
+        getWindow().statusBarColor = Color.parseColor("#f0f0f0") // set status background white
+
     }
 }
